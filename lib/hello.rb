12e55e665fe4e759ea4dbb0569982cfe831397c8
Silvia <sespino1@uncc.edu>
2016-04-13 18:15:37 +0000

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,7 @@
 #Default is "World"
 # Author : Jim Weirich sespino1@uncc.edu
-name = ARGV.first || "World"
-puts "Hello, #{name}!"
+
+puts "What's your name"
+my_name = gets.strip
+
+puts "Hello, #{my_name}!"
